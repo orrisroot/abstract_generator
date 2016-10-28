cae04eee3565aae6b51a6e256ccc514474c38e2b
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2016-10-28 15:26:14 +0900

    change regular expression methods

--- AbstractGenerator.py
+++ AbstractGenerator.py
@@ -6,14 +6,17 @@ by nebula
 Dependency: pandas, python-docx
 """
 import pandas as pd
-from docx import Document
-from docx.shared import Pt
+import docx
+#from docx import Document
+#from docx.shared import Pt
 import re
 
 
 class AbstractGenerator:
     def __init__(self):
         self.records = None
+        self.exreg4super = re.compile(r'(\(\w+\))')
+        self.exreg4italic = re.compile(r'(\<i\>\w+\</i\>)')
 
     def read_xlsx(self, filename):
         print('Reading: %s' % filename)
@@ -23,18 +26,18 @@ class AbstractGenerator:
     def write_docx(self, filename):
         print('Writing: %s' % filename)
 
-        doc = Document()
-        super_match = re.compile(r'(\(\w+\))')
+        doc = docx.Document()
+        
         for i in self.records.index:
 
             # ID + Title
             p = doc.add_paragraph(('P-%03d ' % i) + self.records.title[i])
-            p.runs[0].font.size = Pt(16)
+            p.runs[0].font.size = docx.shared.Pt(16)
             p.runs[0].bold = True
 
             # Authors
             p = doc.add_paragraph()
-            author_list = super_match.split(self.records.authors[i])
+            author_list = self.exreg4super.split(self.records.authors[i])
             for j in range(len(author_list)):
                 if j & 1:
                     p.add_run(author_list[j]).font.superscript = True
@@ -43,7 +46,7 @@ class AbstractGenerator:
 
             # Affiliations
             p = doc.add_paragraph(self.records.affiliations[i])
-            p.runs[0].font.size = Pt(9)
+            p.runs[0].font.size = docx.shared.Pt(9)
             p.runs[0].italic = True
 
             # Abstract Body
