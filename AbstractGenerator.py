e0aa66136a2230fbe5e7df7abcdb3a62debd9992
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2016-10-28 21:20:47 +0900

    using template file BUG: combine file is not work well

--- AbstractGenerator.py
+++ AbstractGenerator.py
@@ -7,9 +7,9 @@ Dependency: pandas, python-docx
 """
 import pandas as pd
 import docx
-#from docx import Document
-#from docx.shared import Pt
+import docxtpl
 import re
+import os
 
 
 class AbstractGenerator:
@@ -17,6 +17,17 @@ class AbstractGenerator:
         self.records = None
         self.exreg4super = re.compile(r'(\(\w+\))')
         self.exreg4italic = re.compile(r'(\<i\>\w+\</i\>)')
+
+    def _insert_image(self, filename, image_filename):
+        doc = docx.Document(filename)
+
+        for paragraph in doc.paragraphs:
+            if '[[FIGURE]]' in paragraph.text:
+                paragraph.text = ''
+                run = paragraph.add_run()
+                run.add_picture(image_filename)
+
+        doc.save(filename)
 
     def read_xlsx(self, filename):
         print('Reading: %s' % filename)
@@ -60,11 +71,73 @@ class AbstractGenerator:
 
         doc.save(filename)
 
+    def write_docx_with_template(self, filename, templatename, template_words, image_col=None, image_dir=''):
+        print('Writing: %s with %s' % (filename, templatename))
+        filename_base, filename_ext = os.path.splitext(filename)
+
+        for i in self.records.index:
+            doc = docxtpl.DocxTemplate(templatename)
+
+            filename_single = str(i) + '.docx'
+            context = {}
+
+            print(self.records.loc[i,:])
+
+            for word in template_words:
+                context[word] = self.records.loc[i, word]
+
+            print(context)
+
+            doc.render(context)
+            doc.save(filename_single)
+
+            if image_col is not None:
+                self._insert_image(filename_single, os.path.join(image_dir, context['FigureFileName']))
+
+        # Joint docx files
+        #files = ['0.docx', '1.docx']
+        #self.combine_word_documents(files)
+
+        doc = docx.Document()
+        for i in self.records.index:
+            filename_single = str(i) + '.docx'
+            doc_single = docx.Document(filename_single)
+            for element in doc_single._body._element:
+                doc._body._element.append(element)
+
+            doc.add_page_break()
+
+        doc.save(filename)
+
+    '''
+    def combine_word_documents(self, files):
+        combined_document = docx.Document()
+        for file in files:
+            sub_doc = docx.Document(file)
+
+            for element in sub_doc._body._element:
+                combined_document._body._element.append(element)
+
+        combined_document.save('combined_word_documents.docx')
+    '''
 
 if __name__ == '__main__':
 
-    abgen = AbstractGenerator()
+    template_words = [
+        u'Title',
+        u'Name',
+        u'Affiliation',
+        u'email',
+        u'ProgramNo',
+        u'DOI',
+        u'Abstract',
+        u'FigureFileName',
+        u'FigureComment'
+    ]
 
-    abgen.read_xlsx('input.xlsx')
-    abgen.write_docx('output.docx')
+
+    abgen = AbstractGenerator()
+    abgen.read_xlsx('./private/aini2016_example.xlsx')
+    # abgen.write_docx('output.docx')
+    abgen.write_docx_with_template('output.docx', 'template.docx', template_words, image_col='FigureFileName', image_dir='private')
 
