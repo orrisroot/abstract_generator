97c72a766280d9ec51f4371a31d1af5ca176b164
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2016-10-30 20:27:06 +0900

    format for AINI2016

--- AbstractGenerator.py
+++ AbstractGenerator.py
@@ -7,14 +7,15 @@ Dependency: pandas, python-docx
 """
 import pandas as pd
 import docx
-import docxtpl
+# import docxtpl
 import re
 import os
 
 
 class AbstractGenerator:
-    def __init__(self):
+    def __init__(self, image_dir=''):
         self.records = None
+        self.image_dir = image_dir
         self.exreg4super = re.compile(r'(\(\w+\))')
         self.exreg4italic = re.compile(r'(\<i\>\w+\</i\>)')
 
@@ -36,112 +37,108 @@ class AbstractGenerator:
         exls = pd.ExcelFile(filename)
         self.records = exls.parse()
 
-    def write_docx(self, filename):
+    def write_docx(self, filename, template=None):
         print('Writing: %s' % filename)
 
-        doc = docx.Document()
-        
+        if template is not None:
+            doc = docx.Document(template)
+        else:
+            doc = docx.Document()
+
         for i in self.records.index:
-
-            # ID + Title
-            p = doc.add_paragraph(('P-%03d ' % i) + self.records.title[i])
-            p.runs[0].font.size = docx.shared.Pt(16)
-            p.runs[0].bold = True
-
-            # Authors
-            p = doc.add_paragraph()
-            author_list = self.exreg4super.split(self.records.authors[i])
-            for j in range(len(author_list)):
-                if j & 1:
-                    p.add_run(author_list[j]).font.superscript = True
-                else:
-                    p.add_run(author_list[j])
-
-            # Affiliations
-            p = doc.add_paragraph(self.records.affiliations[i])
-            p.runs[0].font.size = docx.shared.Pt(9)
-            p.runs[0].italic = True
-
-            # Abstract Body
-            p = doc.add_paragraph(self.records.abstract[i])
-
-            # keywords
-            p = doc.add_paragraph('Keywords: ')
-            p.add_run(self.records.keywords[i]).italic = True
+            # self._write_doc_jscpb2016(doc, self.records.loc[i])
+            self._write_doc_aini2016(doc, self.records.loc[i])
 
             doc.add_page_break()
 
         doc.save(filename)
 
-    def write_docx_with_template(self, filename, templatename, template_words, image_col=None, image_dir=''):
-        print('Writing: %s with %s' % (filename, templatename))
-        filename_base, filename_ext = os.path.splitext(filename)
+    def _write_doc_jscpb2016(self, doc, record):
+        print('"%s"' % record['title'])
 
-        for i in self.records.index:
-            doc = docxtpl.DocxTemplate(templatename)
+        # ID + Title
+        p = doc.add_paragraph(record.title)
+        p.runs[0].font.size = docx.shared.Pt(16)
+        p.runs[0].bold = True
 
-            filename_single = str(i) + '.docx'
-            context = {}
+        # Authors
+        p = doc.add_paragraph()
+        author_list = self.exreg4super.split(record.authors)
+        for j in range(len(author_list)):
+            if j & 1:
+                p.add_run(author_list[j]).font.superscript = True
+            else:
+                p.add_run(author_list[j])
 
-            print(self.records.loc[i,:])
 
-            for word in template_words:
-                context[word] = self.records.loc[i, word]
+        # Affiliations
+        p = doc.add_paragraph(record.affiliations)
+        p.runs[0].font.size = docx.shared.Pt(9)
+        p.runs[0].italic = True
 
-            print(context)
+        # Abstract Body
+        p = doc.add_paragraph(record.abstract)
 
-            doc.render(context)
-            doc.save(filename_single)
+        # keywords
+        p = doc.add_paragraph('Keywords: ')
+        p.add_run(record.keywords).italic = True
 
-            if image_col is not None:
-                self._insert_image(filename_single, os.path.join(image_dir, context['FigureFileName']))
 
-        # Joint docx files
-        #files = ['0.docx', '1.docx']
-        #self.combine_word_documents(files)
 
-        '''
-        doc = docx.Document()
-        for i in self.records.index:
-            filename_single = str(i) + '.docx'
-            doc_single = docx.Document(filename_single)
-            for element in doc_single._body._element:
-                doc._body._element.append(element)
+    def _write_doc_aini2016(self, doc, record):
+        print('"%s"' % record['Title'])
 
-            doc.add_page_break()
+        font = doc.styles['Normal'].font
+        font.size = docx.shared.Pt(10)
+        font.name = 'Lucida Grande'
 
-        doc.save(filename)
-        '''
 
-    '''
-    def combine_word_documents(self, files):
-        combined_document = docx.Document()
-        for file in files:
-            sub_doc = docx.Document(file)
+        # ID + Title
+        p = doc.add_paragraph(record['Title'])
+        p.runs[0].font.size = docx.shared.Pt(12)
+        p.runs[0].bold = True
+        p.runs[0].italic = True
+        p.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+        doc.add_paragraph()
 
-            for element in sub_doc._body._element:
-                combined_document._body._element.append(element)
+        # Authors
+        p = doc.add_paragraph()
+        p.add_run(record['Name']).bold = True
+        p.add_run(record['Affiliation'] + '\n' + record['email'])
+        p.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+        paragraph_format = p.paragraph_format
+        paragraph_format.line_spacing = docx.shared.Pt(12)
+        doc.add_paragraph()
 
-        combined_document.save('combined_word_documents.docx')
-    '''
+        p = doc.add_paragraph(record['ProgramNo'] + '\n' + record['DOI'])
+
+        # Abstract Body
+        p = doc.add_paragraph(record['Abstract'])
+        paragraph_format = p.paragraph_format
+        paragraph_format.line_spacing = docx.shared.Pt(11)
+
+        # Figure
+        doc.add_picture(os.path.join(self.image_dir, record['FigureFileName']))
+        p = doc.add_paragraph(record['FigureComment'])
+        paragraph_format = p.paragraph_format
+        paragraph_format.line_spacing = docx.shared.Pt(11)
+
+        # Citation
+        # split author's affiliation number
+        author_list = self.exreg4super.split(record['Name'])
+        author_tmp = ''
+        for j in range(len(author_list)):
+            if j & 1:
+                pass
+            else:
+                author_tmp += author_list[j]
+        p = doc.add_paragraph('Citation: ' + author_tmp + ', ' + record['Title'] + ', AINI 2016 Abstracts' + ', ' + record['DOI'])
+        paragraph_format = p.paragraph_format
+        paragraph_format.line_spacing = docx.shared.Pt(11)
+
 
 if __name__ == '__main__':
-
-    template_words = [
-        u'Title',
-        u'Name',
-        u'Affiliation',
-        u'email',
-        u'ProgramNo',
-        u'DOI',
-        u'Abstract',
-        u'FigureFileName',
-        u'FigureComment'
-    ]
-
-
-    abgen = AbstractGenerator()
+    abgen = AbstractGenerator(image_dir='./private')
     abgen.read_xlsx('./private/aini2016_example.xlsx')
-    # abgen.write_docx('output.docx')
-    abgen.write_docx_with_template('output.docx', 'private/aini2016_template.docx', template_words, image_col='FigureFileName', image_dir='private')
+    abgen.write_docx('output.docx', './private/aini2016_template2.docx')
 
